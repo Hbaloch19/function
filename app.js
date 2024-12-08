419b893800e0a7499ea731cd68e643636b40158d
hbaloch19 <hanadirafiq1927@gmail.com>
2024-12-08 22:22:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+//without parameter and return:-
+// function sayHello() {
+//     console.log("Hello Hanadi!"); 
+// }
+// sayHello();
+// sayHello();
+
+//with parameter:-
+// function sayHello(name) {
+//     console.log("hey"  + name + "!"); 
+// }
+// sayHello("hanadi");
+// sayHello("maheen");
+
+//function with return value:-
+//Addition
+// function addNumbers(a,b){
+//     return a + b;
+// }
+// let sum = addNumbers(2 , 10);
+// console.log(sum);
+
+//Subtraction
+// function subNumbers(a,b){
+//     return a - b;
+// }
+// let sub = subNumbers(2 , 10);
+// console.log(sub);
+
+//Real life examples:-
+// function calculateTotal(price, taxRate){
+// let tax = price * taxRate;
+// let total = price + tax;
+// return total;
+// }
+// let totalPrice = calculateTotal(30, 0.08);
+// console.log("totalPrice: $"+ totalPrice);
+
+
